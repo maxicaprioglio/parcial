e1d7eeebe19c581ad15f5213130db47713109bdd
Maximiliano Caprioglio <76877022+maxicaprioglio@users.noreply.github.com>
2025-12-17 09:45:13 -0300

    CAMBIOS

--- web/static/web/js/panel.js
+++ web/static/web/js/panel.js
@@ -85,9 +85,6 @@ document.addEventListener("click", function (e) {
     document.getElementById("editarFecha").value = btn.dataset.fecha;
     document.getElementById("editarNombre").value = btn.dataset.nombre;
     document.getElementById("editarApellido").value = btn.dataset.apellido;
-    console.log(btn.dataset.mail);
-    console.log(document.getElementById("editarMail"));
-    console.log(btn.dataset.email);
     document.getElementById("editarMail").value = btn.dataset.mail;
     document.getElementById("editarLinkedin").value = btn.dataset.linkedin;
     document.getElementById("editarMensaje").value = btn.dataset.mensaje;
